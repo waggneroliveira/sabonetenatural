eb12080dbb1e430870b64400ad47245fd0b7a84b
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2024-11-18 09:19:17 -0300

    Update
    Ajuste config vite

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+// import defaultTheme from 'tailwindcss/defaultTheme';
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+//         './storage/framework/views/*.php',
+//         './resources/**/*.blade.php',
+//         './resources/**/*.js',
+//         './resources/**/*.vue',
+//     ],
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+//             },
+//         },
+//     },
+//     plugins: [],
+// };
+
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/views/**/*.blade.php', // Blades no diretório views
+        './resources/js/**/*.vue',         // Apenas arquivos Vue necessários
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -35,4 +35,9 @@ export default defineConfig({
             ]
         })
     ],
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        strictPort: true,
+    },
 });
