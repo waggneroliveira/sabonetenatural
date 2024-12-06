25368ee008b2069e1bf5b90f44ac12adb94d0f77
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2024-12-06 16:25:03 -0300

    Update ProductFilter.vue

--- resources/js/components/ProductFilter.vue
+++ resources/js/components/ProductFilter.vue
@@ -1,24 +1,26 @@
 <template>
-    <div class="box-filter">
-        <div class="box-filter__first">
-            <h2 class="box-filter__title"></h2>
+    <div class="box-filter flex justify-center items-center flex-column">
+        <div class="bg-[#F2F7DE] flex justify-center items-center flex-column w-full max-w-[1172px] h-[249px]">
+            <div class="box-filter__first ">
+                <h2 class="box-filter__title"></h2>
 
-            <form action="" method="get">
-                <div class="label">
-                    <input type="text" name="search" id="search" placeholder="Quero procurar">
-                    <img :src="icon" alt="Ícone serach">
+                <form action="" method="get">
+                    <div class="label">
+                        <input type="text" name="search" id="search" placeholder="Quero procurar">
+                        <img :src="icon" alt="Ícone serach">
+                    </div>
+                </form>
+            </div>
+            <div class="box-filter__second">
+                <div class="box-filter__second-category">
+                    <nav>
+                        <ul class="flex gap-[2.875rem]">
+                            <li class=" border-[1.5px] border-solid border-[#419263]" v-for="(item, index) in items" :key="index">
+                                <a href="" class="text-[#419263] text-[0.813rem] montagu-slab">{{ item.title }}</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-            </form>
-        </div>
-        <div class="box-filter__second">
-            <div class="box-filter__second-category">
-                <nav>
-                    <ul class="flex gap-[2.875rem]">
-                        <li class=" border-[1.5px] border-solid border-[#419263]" v-for="(item, index) in items" :key="index">
-                            <a href="" class="text-[#419263] text-[0.813rem] montagu-slab">{{ item.title }}</a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </div>
     </div>
